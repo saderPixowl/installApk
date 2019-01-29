3ed822f864e5f81ae0b93f79804f63276a31a6e6
sader <sader@pixowl.com>
2019-01-29 19:10:13 -0300

    inital commit

---
+++ adbCommands.py
@@ -0,0 +1,44 @@
+import subprocess
+import os
+import time
+
+def run_command(command):
+    print ("run command adb " + command + "\noutput:")
+    p = subprocess.Popen((ADB_PATH + command).split(),
+                         stdout=subprocess.PIPE,
+                         stderr=subprocess.STDOUT)
+    for line in iter(p.stdout.readline, b''):
+        print(line.decode('utf-8'))
+
+    print ("end command output ")
+
+
+ADB_PATH = "/Users/alejandro/Library/Android/sdk/platform-tools/adb "
+PACKAGE_NAME = "com.activision.peanuts"
+APK_PATH = "/Users/alejandro/Downloads/"
+APK_NAME = "Peanuts-499-qa-googleplay-v7a-activision-release.apk"
+
+commandDevices = "devices"
+commandUnistall = "uninstall " + PACKAGE_NAME
+commandInstall = "install " + APK_PATH + APK_NAME
+commandPlay = "shell monkey -p " + PACKAGE_NAME + " -c android.intent.category.LAUNCHER 1"
+commandStop = "shell am force-stop " + PACKAGE_NAME
+
+minToSleep = 1/20
+iterations = 4
+run_command(commandStop)
+#run_command(commandUnistall)
+#run_command(commandInstall)
+
+   
+for i in range(iterations):
+    run_command(commandPlay)
+    time.sleep(minToSleep * 60.0)
+    run_command(commandStop)
+
+print ("END adbCommand.py")
+
+
+
+
+  
